d65b15cb28fbe7d28c1134bfe6ddef6492a76e7f
Patrycja Pyrek <51048502+PatrycjaPyrek@users.noreply.github.com>
2020-12-05 18:03:07 +0100

    Add files via upload

---
+++ ImplementacjaStosu/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Stos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StosWTablicy<string> s = new StosWTablicy<string>(2);
+            s.Push("km");
+            s.Push("aa");
+            s.Push("xx");
+            s.Push("sa");
+            s.TrimExcess();
+            foreach (var x in s.ToArray())
+                Console.WriteLine(x);
+
+            Console.WriteLine();
+            Console.WriteLine(s.Count);
+        }
+    }
+}
